78b06b3923f9e96766cd37b92d402cc75dceb41c
dev-koushal <koushalydv99@gmail.com>
2026-02-23 18:56:00 +0530

    fix controllerUserd

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,90 +1,102 @@
-import validator from 'validator';
-import bcrypt, { truncates } from 'bcryptjs';
-import { genToken } from '../config/token.js';
-import User from '../models/userModel.js'
+import validator from "validator";
+import bcrypt from "bcryptjs";
+import { genToken } from "../config/token.js";
+import User from "../models/userModel.js";
 
+export const signUp = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
 
-export const signUp = async(req,res)=>{
-   try {
-     const {name, email,password, role } = req.body;
-    let existingUser = await User.findOne({email});
-    if(existingUser){
-        return res.status(400).json({message:"User already exist!"});
+    if (!name || !email || !password || !role) {
+      return res.status(400).json({
+        message: "All fields need to filled!",
+      });
     }
-    if(!validator.isEmail(email)){
-        return res.status(400).json({message:"Email should be valid!"});
+    let existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exist!" });
     }
-    if(password.length<8){
-        return res.status(400).json({message:"Password need atleast 8 characters!"});
+    if (!validator.isEmail(email)) {
+      return res.status(400).json({ message: "Email should be valid!" });
     }
-    let hashPassword = await bcrypt.hash(password,10);
+    if (password.length < 8) {
+      return res
+        .status(400)
+        .json({ message: "Password need atleast 8 characters!" });
+    }
+    let hashPassword = await bcrypt.hash(password, 10);
 
     const user = await User.create({
-        name,
-        email,
-        password:hashPassword,
-        role
-    })
-    
+      name,
+      email,
+      password: hashPassword,
+      role,
+    });
 
-    let token = await genToken(user._id)
+    let token = await genToken(user._id);
 
-    res.cookie("token",token,{
-        httpOnly:true,
-        secure:false, //need to update as now it is running on the local machine
-        samesite:"Strict",
-        maxAge:7*24*60*60*1000 
-    })
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
 
     const safeUser = user.toObject();
     delete safeUser.password;
-    return res.status(201).json({success:true,message:"User created successfully!",user:safeUser});
+    return res
+      .status(201)
+      .json({
+        success: true,
+        message: "User created successfully!",
+        user: safeUser,
+      });
+  } catch (error) {
+    return res.status(400).json({ message: `SignUp error ${error}` });
+  }
+};
 
-   } catch (error) {
-     return res.status(400).json({message:`SignUp error ${error}`})
-   }
-}
+export const login = async (req, res) => {
+  try {
+    const { email, password, role } = req.body;
+    let user = await User.findOne({ email }).select("+password");
 
-export const login = async (req,res)=>{
-    try {
-        const{email,password,role} =req.body;
-        let user = await User.findOne({email}).select("+password");
-
-        if(!user){
-            return res.status(400).json({message:"User not register!"})
-        }
-
-        if(role!= user.role ){
-            return res.status(400).json({message:"Role doesn't match!"})
-        }
-       
-        const isMatch = await bcrypt.compare(password,user.password);
-       
-        if(!isMatch){
-            return res.status(400).json({message:"Wrong password!"})
-        }
-
-        let token = await genToken(user._id);
-        
-        res.cookie("token",token,{
-        httpOnly:true,
-        secure:false, //need to update as now it is running on the local machine
-        samesite:"strict",
-        maxAge:7*24*60*60*1000 
-    })
-        user.password = undefined;
-        return res.status(200).json({success: true,message: "Login successful!",user});
-
-    } catch (error) {
-         return res.status(400).json({message:`Login error ${error}`})
+    if (!user) {
+      return res.status(400).json({ message: "User not register!" });
     }
-}
 
-export const logout = async (req,res) => {
-    try {
-       await res.clearCookie("token")
-       return res.status(200).json({message:"Logged successfully!"})
-    } catch (error) {
-        return res.status(400).json({message:`Logout error ${error}`})
+    if (role != user.role) {
+      return res.status(400).json({ message: "Role doesn't match!" });
     }
-}
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Wrong password!" });
+    }
+
+    let token = await genToken(user._id);
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    user.password = undefined;
+    return res
+      .status(200)
+      .json({ success: true, message: "Login successful!", user });
+  } catch (error) {
+    return res.status(400).json({ message: `Login error ${error}` });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    await res.clearCookie("token");
+    return res.status(200).json({ message: "Logged successfully!" });
+  } catch (error) {
+    return res.status(400).json({ message: `Logout error ${error}` });
+  }
+};
