a55e89e2eb08d6e9aa332d94064b4270e9007623
dev-koushal <koushalydv99@gmail.com>
2026-02-24 19:48:05 +0530

    home intital

--- client/src/assets/pages/Home.jsx
+++ client/src/assets/pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from '../../components/Navbar'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='w-full overflow-hidden'>
+      <div className='w-full md:h-[124vh] h-[70vh]'>
+        <Navbar/>
+      </div>
+    </div>
   )
 }
 
