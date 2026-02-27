2411581f85268b627c073dcf595a48c2746a9baa
dev-koushal <koushalydv99@gmail.com>
2026-02-27 12:35:09 +0530

    minor

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "/skillio.png";
-
+import {User2Icon} from 'lucide-react'
 function Navbar() {
   return (
     <>
@@ -13,22 +13,46 @@ function Navbar() {
               className="h-14 w-full object-contain mb-2"
             />
           </div>
-          <div className="flex gap-4">
+
+          
+          <div className="flex gap-4 items-center">
             <div
-              id="dashboard-btn"
+              className="text-lg border-2 border-cyan-900 rounded-full items-center justify-center p-1.5 hover:bg-gray-600/90 cursor-pointer  "
+            >
+                <User2Icon size={30} color="#006075"/>
+            </div>
+
+
+
+            <div
+              className="text-lg rounded-2xl p-2 items-center justify-center "
+            >
+              <button
+                type="button"
+                className="text-white border-cyan-900 hover:bg-gray-600/90 focus:ring-4 focus:ring-cyan-900/50 box-border border-2  focus:outline-none font-medium leading-5 rounded-base text-sm px-4 py-2.5 text-center inline-flex items-center dark:focus:ring-cyan-900/50 cursor-pointer rounded-sm"
+              >
+                Dashboard
+              </button>
+            </div>
+            <div
               className="text-lg rounded-2xl p-2 items-center justify-center "
             >
               <button
                 type="button"
                 className="text-white bg-cyan-900 hover:bg-gray-600/90 focus:ring-4 focus:ring-cyan-900/50 box-border border border-transparent focus:outline-none font-medium leading-5 rounded-base text-sm px-4 py-2.5 text-center inline-flex items-center dark:focus:ring-cyan-900/50 cursor-pointer"
               >
-                Dashboard
+                Login
               </button>
             </div>
-            <div className="h-11 w-12 rounded-full flex bg-cyan-50 mt-1.5">
-              <p className="items-center justify-center mx-auto text-4xl text-black font-bold mt-1">
-                K
-              </p>
+            <div
+              className="text-lg rounded-2xl p-2 items-center justify-center "
+            >
+              <button
+                type="button"
+                className="text-white bg-cyan-900 hover:bg-gray-600/90 focus:ring-4 focus:ring-cyan-900/50 box-border border border-transparent focus:outline-none font-medium leading-5 rounded-base text-sm px-4 py-2.5 text-center inline-flex items-center dark:focus:ring-cyan-900/50 cursor-pointer"
+              >
+                Logout
+              </button>
             </div>
           </div>
         </div>
