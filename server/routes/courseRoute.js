b37d665f7c54e8b7015c534767c719202e99b977
dev-koushal <koushalydv99@gmail.com>
2026-03-09 18:54:27 +0530

    courseRoute

---
+++ server/routes/courseRoute.js
@@ -0,0 +1,15 @@
+import express, { Router } from 'express'
+import {createCourse, editCourse, getCreatorCourses, getPublishedCourses, removeCourse} from '../controller/courseController'
+import isAuth from '../middleware/isAuth.js'
+import upload from '../middleware/multer.js'
+const courseRouter = Router();
+
+courseRouter.post("/create",isAuth, createCourse)
+courseRouter.get("/getpublished",isAuth, getPublishedCourses)
+courseRouter.get("/getcreator",isAuth, getCreatorCourses)
+courseRouter.post("/editcourse/:courseId",isAuth, upload.single("thumbnail"),editCourse)
+courseRouter.get("/getcoursebyid/:courseId",isAuth, getCourseById)
+courseRouter.delete("/remove/:courseId",isAuth, removeCourse)
+
+
+export default courseRouter;
