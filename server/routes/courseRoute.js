8ecf5d116a15eaeea1fd71d85ab981fa312c1ccd
dev-koushal <koushalydv99@gmail.com>
2026-03-13 15:35:41 +0530

    create routes for the lectures

--- server/routes/courseRoute.js
+++ server/routes/courseRoute.js
@@ -1,5 +1,5 @@
 import express, { Router } from 'express'
-import {createCourse, editCourse, getCourseById, getCreatorCourses, getPublishedCourses, removeCourse} from '../controller/courseController.js'
+import {createCourse, createLecture, editCourse, editLecture, getCourseById, getCourseLecture, getCreatorCourses, getPublishedCourses, removeCourse, removeLecture} from '../controller/courseController.js'
 import isAuth from '../middleware/isAuth.js'
 import upload from '../middleware/multer.js'
 const courseRouter = Router();
@@ -11,5 +11,12 @@ courseRouter.post("/editcourse/:courseId",isAuth, upload.single("thumbnail"),edi
 courseRouter.get("/getcourse/:courseId",isAuth, getCourseById)
 courseRouter.delete("/remove/:courseId",isAuth, removeCourse)
 
+
+// for lectures
+
+courseRouter.post("/createlecture/:courseId",isAuth,createLecture)
+courseRouter.get("/courselecture/:courseId",isAuth,getCourseLecture)
+courseRouter.post("/editlecture/:lectureId",isAuth,upload.single("videoUrl"),editLecture)
+courseRouter.delete("/removelecture/:lectureId",isAuth,removeLecture)
 
 export default courseRouter;
