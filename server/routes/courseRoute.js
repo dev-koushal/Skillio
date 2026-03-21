d12a87bbbe7def19a33066f6a39db599eadadbc6
dev-koushal <koushalydv99@gmail.com>
2026-03-21 10:26:39 +0530

    Axios posting

--- server/routes/courseRoute.js
+++ server/routes/courseRoute.js
@@ -1,5 +1,5 @@
 import express, { Router } from 'express'
-import {createCourse, createLecture, editCourse, editLecture, getCourseById, getCourseLecture, getCreatorCourses, getPublishedCourses, removeCourse, removeLecture} from '../controller/courseController.js'
+import {createCourse, createLecture, editCourse, editLecture, getCourseById, getCourseLecture, getCreatorById, getCreatorCourses, getPublishedCourses, removeCourse, removeLecture} from '../controller/courseController.js'
 import isAuth from '../middleware/isAuth.js'
 import upload from '../middleware/multer.js'
 const courseRouter = Router();
@@ -18,5 +18,9 @@ courseRouter.post("/createlecture/:courseId",isAuth,createLecture)
 courseRouter.get("/courselecture/:courseId",isAuth,getCourseLecture)
 courseRouter.post("/editlecture/:lectureId",isAuth,upload.single("videoUrl"),editLecture)
 courseRouter.delete("/removelecture/:lectureId",isAuth,removeLecture)
+
+// creatorgetting
+
+courseRouter.post("/creator" ,isAuth,getCreatorById)
 
 export default courseRouter;
