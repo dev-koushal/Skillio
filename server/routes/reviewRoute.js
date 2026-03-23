7e34136055312ec2696b7e6b0d3655a404723782
dev-koushal <koushalydv99@gmail.com>
2026-03-23 11:32:05 +0530

    routing

---
+++ server/routes/reviewRoute.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import { createReview, getReviews } from '../controller/reviewContoller.js'
+import isAuth from '../middleware/isAuth.js'
+
+const reviewRouter = express.Router()
+
+reviewRouter.post("/createreview",isAuth,createReview)
+reviewRouter.get("/getreview",getReviews)
+
+export default reviewRouter
