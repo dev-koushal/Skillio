d407de4c623877921e2f02cef50652fab268bfc2
Koushal Yadav <137811402+dev-koushal@users.noreply.github.com>
2026-03-24 13:20:52 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT  ||8000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://skillio-1-bb42.onrender.com",
     credentials:true,    
 }))
 app.use("/api/auth",authRouter);
